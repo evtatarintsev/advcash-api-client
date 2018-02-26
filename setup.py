68dac73634b5ad74d105cadb64cd6aa1c49fe0f0
Evgeny Tatarintsev <evtatarintsev@ya.ru>
2018-02-26 13:27:38 +0300

    add setup script

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+__author__ = 'Evgeny Tatarintsev'
+__version__ = '0.1'
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='advcash-api-client',
+    version=__version__,
+    packages=find_packages(),
+    url='https://github.com/evtatarintsev/advcash-api-client',
+    license='MIT',
+    author=__author__,
+    author_email='evtatarintsev@ya.ru',
+    keywords=['advcash', 'api', 'client'],
+    description='Integrating django project with yandex-money',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'zeep>=2.5.0',
+    ],
+)
